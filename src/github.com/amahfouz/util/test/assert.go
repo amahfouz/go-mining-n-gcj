f9132687f7a95dd28b3c0fdd5e0a5b672b562f02
Ayman Mahfouz <amahfouz@webalo.com>
2015-07-06 18:09:43 -0700

    Added assert non-nil and asser panic-recover.

--- src/github.com/amahfouz/util/test/assert.go
+++ src/github.com/amahfouz/util/test/assert.go
@@ -2,8 +2,30 @@ package test
 
 import "testing"
 
+// marker interface
+type NonNillable interface{}
+
 func Assert(t *testing.T, condition bool, errMsg string) {
-    if ! condition {
-        t.Error(errMsg)
-    }
+	if !condition {
+		t.Error(errMsg)
+	}
+}
+
+func AssertNonNil(t *testing.T, nonNullObj NonNillable) {
+	if nonNullObj == nil {
+		t.Error("Unexpected null.")
+	}
+}
+
+func AssertFail(t *testing.T, fn func(), errMsg string) {
+	defer func() {
+		if r := recover(); r != nil {
+			// failure occured as expected
+		} else {
+			t.Error(errMsg)
+		}
+	}()
+
+	// call the function that is expected to fail
+	fn()
 }
